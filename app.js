e9f597b472ba102215286318f684ab1694b567de
brenovit <breno.arantes97@gmail.com>
2019-05-22 17:37:49 -0300

    Criação de componentes

---
+++ app.js
@@ -0,0 +1,56 @@
+Vue.component("click-counter", {
+  template: '<button @click="count++">{{count}}</button>',
+  data() {
+    return {
+      count: 0
+    };
+  }
+});
+
+new Vue({
+  el: "#root",
+  data: {
+    header: "shopping list",
+    newItem: "",
+    state: "default",
+    shopItems: [
+      {
+        label: "10 cokes",
+        purchased: false
+      },
+      {
+        label: "5 hats",
+        purchased: true
+      },
+      {
+        label: "2 bananas",
+        purchased: false
+      }
+    ]
+  },
+  computed: {
+    reversedItems() {
+      return this.shopItems.slice(0).reverse();
+    }
+  },
+  methods: {
+    addItem() {
+      var item = {
+        label: this.newItem,
+        purchased: false
+      };
+      this.shopItems.push(item);
+      this.cleanNewItem();
+    },
+    changeState(newState) {
+      this.state = newState;
+      this.cleanNewItem();
+    },
+    cleanNewItem() {
+      this.newItem = "";
+    },
+    togglePurchased(item) {
+      item.purchased = !item.purchased;
+    }
+  }
+});
