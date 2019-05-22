1f1b636e6f4eebc6c3433c0f467334b91e3b448f
brenovit <breno.arantes97@gmail.com>
2019-05-22 18:29:57 -0300

    Criação de componentes Uso de props

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,5 +1,34 @@
-Vue.component("", {});
+Vue.component("plan", {
+  template: "#plan-template",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    price: Number
+  }
+});
 
 new Vue({
-  el: "#app"
+  el: "#app",
+  data: {
+    plans: [
+      {
+        name: "The Hacker",
+        price: 5.0
+      },
+      {
+        name: "The Single",
+        price: 10.0
+      },
+      {
+        name: "The Curios",
+        price: 12.0
+      },
+      {
+        name: "The Addict",
+        price: 17.0
+      }
+    ]
+  }
 });
